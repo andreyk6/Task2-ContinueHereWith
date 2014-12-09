e8d6097900b41c9455de40c51f7d3bd3daba81b3
Andrii <andreyk6@gmail.com>
2014-12-10 00:08:41 +0200

    Project submit

---
+++ Task2/Task2/ContinueHereWithExtension.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class ContinueHereWithExtension
+    {
+        #region Action & Action<Task>
+        public static Task ContinueHereWith(this Task task, Action continuationAction)
+        {
+            return task
+                .ContinueWith(
+                    (t) => new Task(continuationAction).Start(),
+                    TaskScheduler.FromCurrentSynchronizationContext()
+                );
+        }
+
+        public static Task ContinueHereWith(this Task task, Action<Task> continuationAction)
+        {
+            return task
+                .ContinueWith(
+                    continuationAction,
+                    TaskScheduler.FromCurrentSynchronizationContext()
+                );
+        }
+        #endregion
+
+        #region Func<T>, Func<Task,T>, Func<Task,Task<T>>
+        public static Task<T> ContinueHereWith<T>(this Task<T> task, Func<T> continuationFunc)
+        {
+            return task
+                .ContinueWith(
+                (t) => continuationFunc.Invoke(), 
+                TaskScheduler.FromCurrentSynchronizationContext()
+                );
+        }
+            
+        public static Task<T> ContinueHereWith<T>(this Task<T> task, Func<Task, T> continuationFunc)
+        {
+            return task
+                .ContinueWith(
+                (t) => continuationFunc.Invoke(t),
+                TaskScheduler.FromCurrentSynchronizationContext()
+                );
+        }
+
+        public static Task<T> ContinueHereWith<T>(this Task<T> task, Func<Task, Task<T>> continuationFunc)
+        {
+            return task
+                .ContinueWith<T>(
+                (t) => continuationFunc.Invoke(t).Result, 
+                TaskScheduler.FromCurrentSynchronizationContext()
+                );
+        }
+        #endregion
+    }
+}
---
+++ Task2/Task2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContinueHereTest();
+        }
+
+        public static void ContinueHereTest()
+        {
+            Console.WriteLine("UI ThreadID - " + Thread.CurrentThread.ManagedThreadId);
+
+            Action mainTaskAction = () => Console.WriteLine("MainTask ThreadID - " + Thread.CurrentThread.ManagedThreadId);
+
+            Action mainContinuationAction = new Action(() => Console.WriteLine("MainAction ThreadID - " + Thread.CurrentThread.ManagedThreadId));
+
+            Action<Task> mainContinuationActionWithTask = new Action<Task>((t) =>
+            {
+                Console.WriteLine("MainAction<Task> ThreadID - " + Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("MainAction<Task> Task.IsCompleted - " + t.IsCompleted);
+            });
+
+            #region Action
+            Task task1 = new Task(mainTaskAction);
+
+            task1.ContinueHereWith(mainContinuationAction);
+
+            task1.Start();
+            task1.Wait();
+            #endregion
+
+            #region Action<Task>
+            Task task2 = new Task(mainTaskAction);
+
+            task2.ContinueWith((t) => Console.WriteLine("Task ContinueWith() action...."));
+
+            task2.ContinueHereWith(mainContinuationActionWithTask);
+
+            task2.Start();
+            task2.Wait();
+            #endregion
+        }
+    }
+}
